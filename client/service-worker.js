f9c92fef003542644e3855abc7df7cc52e9985da
Wout Schoovaerts <wout@designisdead.com>
2018-11-19 16:42:33 +0100

    implementing caching strategies

--- client/service-worker.js
+++ client/service-worker.js
@@ -1,29 +1,71 @@
-const CACHE_NAME = 'vid-voter';
-const URLS_TO_CACHE = [
-    "/offline.html",
+const CACHE_NAME = 'vid-voter-v2';
+const URLS_CACHE_ONLY = [
     "/css/bootstrap.min.css",
-    "/css/cover.css",
-    "/image/cover.jpg"
+    "/js/bootstrap.min.js",
+    "/js/jquery-3.3.1.min.js",
+    "/image/cover.jpg",
+    "/image/did-logo.png",
+    "https://use.fontawesome.com/releases/v5.3.1/css/all.css",
 ];
 
-self.addEventListener("install", function(event) {
+const URLS_OVER_NETWORK_WITH_CACHE_FALLBACK = [
+    "/index.html",
+    "/voter.html",
+    "http://localhost:3000/videos"
+];
+
+self.addEventListener("install", function (event) {
     event.waitUntil(
-        caches.open(CACHE_NAME).then(function(cache) {
-            return cache.addAll(URLS_TO_CACHE);
+        caches.open(CACHE_NAME).then(function (cache) {
+            return cache.addAll(URLS_CACHE_ONLY.concat(URLS_OVER_NETWORK_WITH_CACHE_FALLBACK));
         })
     );
 });
 
-self.addEventListener("fetch", function(event) {
-    event.respondWith(
-        fetch(event.request).catch(function() {
-            return caches.match(event.request).then(function(response) {
-                if (response) {
-                    return response;
-                } else if (event.request.headers.get("accept").includes("text/html")) {
-                    return caches.match("/offline.html");
-                }
-            });
-        })
-    );
+self.addEventListener("fetch", function (event) {
+    const requestURL = new URL(event.request.url);
+
+    if (URLS_OVER_NETWORK_WITH_CACHE_FALLBACK.includes(requestURL.href) || URLS_OVER_NETWORK_WITH_CACHE_FALLBACK.includes(requestURL.pathname)) {
+        event.respondWith(getByNetworkFallingBackByCache(event.request));
+    } else if (URLS_CACHE_ONLY.includes(requestURL.href) || URLS_CACHE_ONLY.includes(requestURL.pathname)) {
+        event.respondWith(getByCacheOnly(event.request));
+    }
 });
+
+/**
+ * 1. We fetch the request over the network
+ * 2. If successful we add the new response to the cache
+ * 3. If failed we return the result from the cache
+ *
+ * @param request
+ * @param showAlert
+ * @returns Promise
+ */
+const getByNetworkFallingBackByCache = (request, showAlert = false) => {
+    return caches.open(CACHE_NAME).then((cache) => {
+        return fetch(request).then((networkResponse) => {
+            cache.put(request, networkResponse.clone());
+            return networkResponse;
+        }).catch(() => {
+            if (showAlert) {
+                alert('You are in offline mode. The data may be outdated.')
+            }
+
+            return caches.match(request);
+        });
+    });
+};
+
+/**
+ * Get from cache
+ *
+ * @param request
+ * @returns Promise
+ */
+const getByCacheOnly = (request) => {
+    return caches.open(CACHE_NAME).then((cache) => {
+       return cache.match(request).then((response) => {
+           return response;
+       });
+    });
+};
